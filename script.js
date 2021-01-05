81e4a5874aa168d383b7ef3840af166e444c777d
= <=>
2021-01-05 14:27:18 +0500

    add alert message on submit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 var submitButton = document.querySelector("button[type='submit']");
+var closeButton = document.querySelector("#close");
+var afterSubmit = document.querySelector("#after-submit");
 submitButton.onclick = (e) => {
     e.preventDefault();
+    afterSubmit.style.display = "flex";
+    afterSubmit.style.left = "0";
+}
+closeButton.onclick = (e) => {
+    afterSubmit.style.display = "none";
+    afterSubmit.style.left = "-100000%";
 }
